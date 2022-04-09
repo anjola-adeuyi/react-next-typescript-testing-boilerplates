88443b80b68ec6c73e804fe15b5cd51d911ef797
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-10 00:40:35 +0100

    add Counter component and fix global css issues

---
+++ src/pages/counter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Counter from "../components/Counter/Counter";
+
+const counter = () => {
+  return <Counter defaultCount={0} description="My Counter" />;
+};
+
+export default counter;
